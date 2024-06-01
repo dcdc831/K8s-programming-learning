aeaa180af543dd3cc449dccd78b5e3cbf57836fc
YulinLi <1826549123@qq.com>
2024-06-01 10:52:59 +0800

    code-generator-demo and crd demo.

---
+++ code-generator-demo/lyl/operator-crd/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	clientset "lyl/operator-crd/pkg/generated/clientset/versioned"
+	"lyl/operator-crd/pkg/generated/informers/externalversions"
+
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/tools/cache"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+func main() {
+	config, err := clientcmd.BuildConfigFromFlags("", clientcmd.RecommendedHomeFile)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	clientset, err := clientset.NewForConfig(config)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	list, err := clientset.CrdV1().Foos("default").List(context.TODO(), v1.ListOptions{})
+	if err != nil {
+		log.Fatalln(err)
+	}
+	for _, foo := range list.Items {
+		println(foo.Name)
+	}
+
+	factory := externalversions.NewSharedInformerFactory(clientset, 0)
+	factory.Crd().V1().Foos().Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc: func(obj interface{}) {
+		},
+	})
+}
